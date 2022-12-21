470de147faf86e634408aa90253e268bd3918c41
Claire Castan <ccastan@live.fr>
2022-12-21 10:19:19 +0100

    :sparkles: Création d'un fichier product.js + récupération de l'id de la page + retour des valeurs de l'api pour chaque produit

---
+++ front/js/product.js
@@ -0,0 +1,59 @@
+let urlId = new URL(location.href).searchParams.get("id");
+let url = `http://localhost:3000/api/products`;
+let productImg = document.getElementsByClassName("item__img");
+
+function getId(url) {
+    //fetch = récupérer infos de l'(url)
+    fetch(url)
+    //pour etre sur qu'on a récupéré les infos
+    .then(function(response){
+        if (response.ok) {
+            return response.json();
+            
+        } 
+    })
+    //utiliser les infos 
+    .then(function(value) {
+        for(let arrayApi of value) { 
+            //si items._id(de l'array de l'api est strictement = à l'urlId (urlsearchparams))
+            if (arrayApi._id == urlId) {
+                //alors
+                //création de l'élement "img", avec sa source et son attribut, filliation
+                let itemsImg = document.createElement("img");
+                itemsImg.setAttribute("src", `${arrayApi.imageUrl}`);
+                itemsImg.setAttribute("alt", `${arrayApi.altTxt}`);
+                productImg[0].appendChild(itemsImg);
+                //création d'un texte pour l'id "title"
+                let productTitle = document.getElementById("title");
+                productTitle.textContent = `${arrayApi.name}`; 
+                //création d'un texte pour l'id "price"
+                let productPrice = document.getElementById("price");
+                productPrice.textContent = `${arrayApi.price}`;
+                //création d'un texte pour l'id "description"
+                let productTxt = document.getElementById("description");
+                productTxt.textContent = `${arrayApi.description}`;
+                //constante qui récupère les couleurs de l'api en fonction de l'id de la page
+                const nameOfColors = arrayApi.colors;
+                //boucle pour générer le tableau du choix de couleurs 
+                for (let valueColors of nameOfColors) {
+                    //création d'un élément <option> pour chaque valeur de couleur, filliation
+                    let productColors = document.getElementById("colors");
+                    let itemsColors = document.createElement("option");
+                    itemsColors.setAttribute("value", valueColors);
+                    itemsColors.textContent = valueColors;
+                    productColors.appendChild(itemsColors);
+                }
+            }
+        }
+    })
+    //en cas si l'appel marche pas 
+    .catch(function(error) {
+        console.log(error);
+    });
+
+}
+getId(url)
+
+
+
+
