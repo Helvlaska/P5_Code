51b6532c093e7473d247f453ada480f11b503220
Claire Castan <ccastan@live.fr>
2023-03-04 13:03:58 +0100

    :sparkles:(confirmation.js) Ajout d'une fonction pour récupérer l'orderId dans l'url et l'afficher sur la page

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,1 +1,7 @@
-let urlId = new URL(location.href).searchParams.get("id");
+function commandNumber(){
+    let getOrderId = new URL(location.href).searchParams.get("orderId");
+    let numbConfirm = document.getElementById("orderId");
+    numbConfirm.setAttribute("value", getOrderId);
+    numbConfirm.textContent = getOrderId;
+}
+commandNumber();
