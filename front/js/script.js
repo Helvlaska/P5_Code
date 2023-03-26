da12f0fc7115aff837d2e16d7c1a2aa1a8f23e06
Claire Castan <ccastan@live.fr>
2023-03-26 21:33:48 +0200

    :sparkles: (script.js) Ajout de commentaires de code

--- front/js/script.js
+++ front/js/script.js
@@ -1,45 +1,66 @@
+// Adresse url de l'API
 const url = "http://localhost:3000/api/products";
-const itemsBloc = document.getElementById("items");
+// Variable pour pointer la section avec l'id "items"
+let itemsBloc = document.getElementById("items");
 
+// Création d'une fonction pour faire appel à l'Api
 function getData(url){
     fetch(url)
+    // Appel à l'api
         .then(function(response){
+            //si l'appel à l'Api est ok
             if (response.ok) {
+                //renvoyer les données en format Json
                 let data = response.json();
                 return data;
             }  
         })
+        // traitement des donées récupérées
         .then(function(value) {
-
+            // Création d'une boucle qui va parcourir l'api
             for(let items of value) {
-                console.log(items)
+                //création d'un bloc html via javascript
+                //création d'un élément <a> (lien)
                 let itemsLink = document.createElement("a");
+                //création d'un attribut "href" + (valeur = page produit de l'item via son Id par la boucle)
                 itemsLink.setAttribute("href", `./product.html?id=${items._id}`);
+                //le lien créé devient enfant de itemsBloc
                 itemsBloc.appendChild(itemsLink);
-
+                //création d'un élément <article>
                 let itemsArticle = document.createElement("article");
+                //l'article crée devient enfant du lien précédement crée
                 itemsLink.appendChild(itemsArticle);
-
+                //création d'un élément <image>
                 let itemsImg = document.createElement("img");
+                //Attribut "src" à l'élément <image> +(valeur = variable de l'image par la boucle)
                 itemsImg.setAttribute("src", `${items.imageUrl}`);
+                //Attribut "alt" à l'élément <image> +(valeur = variable de déscription de l'image par la boucle)
                 itemsImg.setAttribute("alt", `${items.altTxt}`);
+                //l'élément image crée devient enfant de l'article précédement crée
                 itemsArticle.appendChild(itemsImg);
-
+                //création d'un élément <titre>
                 let itemsTitle = document.createElement("h3");
+                //Ajout d'une class à l'élément
                 itemsTitle.classList.add("productName");
+                //Ajout d'un contenu texte déterminé par la boucle dans l'élément titre
                 itemsTitle.textContent = `${items.name}`;
+                //L'élément titre devient enfant de l'élément article
                 itemsArticle.appendChild(itemsTitle);
-
+                //création d'un élément <paragraphe>
                 let itemsTxt = document.createElement("p");
+                //Ajout d'une class à l'élément
                 itemsTxt.classList.add("productDescription");
+                //Ajout d'un contenu texte déterminé par la boucle dans l'élément paragraphe (description)
                 itemsTxt.textContent = `${items.description}`;
+                //L'élément <p> devient enfant de l'élément article
                 itemsArticle.appendChild(itemsTxt);
             }
             
         })
+        //si l'appel à l'Api échoue, une erreur aparait en console
         .catch(function(error) {
             console.log(error);
         });
 }
-
+//Appel de la fonction d'Appel à l'Api et le traitement des données récupérées
 getData(url)
