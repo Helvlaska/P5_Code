2fa5e572c64d22c2299c4ebd38b01d26cd3437d6
Claire Castan <ccastan@live.fr>
2023-04-06 15:57:17 +0200

    :recycle:(product.js) Changement de let en var pour une portabilité hors fonction + Ajout de commentaires

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,7 @@
-let urlId = new URL(location.href).searchParams.get("id");
-let url = `http://localhost:3000/api/products`;
-let productImg = document.getElementsByClassName("item__img");
+//Adresse url de l'api
+const url = `http://localhost:3000/api/products`;
+//Variable pour récupérer l'id du produit séléctionné
+var urlId = new URL(location.href).searchParams.get("id");
 
 function getId(url) {
     //fetch = récupérer infos de l'(url)
@@ -11,35 +12,48 @@ function getId(url) {
             return response.json();
         } 
     })
-    //utiliser les infos 
+    // traitement des donées récupérées 
     .then(function(value) {
+        //Variable pour pointer sur l'élément html qui recevra le contenu descriptif du canapé
+        let productImg = document.getElementsByClassName("item__img");
+        //Création d'une boucle qui va parcourir l'api
         for(let arrayApi of value) { 
             //si items._id(de l'array de l'api est strictement = à l'urlId (urlsearchparams))
             if (arrayApi._id == urlId) {
                 //alors
-                //création de l'élement "img", avec sa source et son attribut, filliation
+                //Variable pour créer un nouvel élément html <img>
                 let itemsImg = document.createElement("img");
+                //Attribution d'une src et sa valeur à <img>
                 itemsImg.setAttribute("src", `${arrayApi.imageUrl}`);
+                //Attribution d'un alt et sa valeur à <img>
                 itemsImg.setAttribute("alt", `${arrayApi.altTxt}`);
+                //Filliation à productImg : itemsImg devient enfant de productImg
                 productImg[0].appendChild(itemsImg);
-                //création d'un texte pour l'id "title"
+                //Variable pour pointer sur l'élément html avec l'id "title"
                 let productTitle = document.getElementById("title");
+                //Attribution d'un contenu de type texte dans l'élément html
                 productTitle.textContent = `${arrayApi.name}`; 
-                //création d'un texte pour l'id "price"
+                //Variable pour pointer sur l'élément html avec l'id "price"
                 let productPrice = document.getElementById("price");
+                //Attribution d'un contenu de type texte dans l'élément html
                 productPrice.textContent = `${arrayApi.price}`;
-                //création d'un texte pour l'id "description"
+                //Variable pour pointer sur l'élément html avec l'id "description"
                 let productTxt = document.getElementById("description");
+                //Attribution d'un contenu de type texte dans l'élément html
                 productTxt.textContent = `${arrayApi.description}`;
                 //constante qui récupère les couleurs de l'api en fonction de l'id de la page
                 const nameOfColors = arrayApi.colors;
                 //boucle pour générer le tableau du choix de couleurs 
                 for (let valueColors of nameOfColors) {
-                    //création d'un élément <option> pour chaque valeur de couleur, filliation
+                    //Variable pour pointer sur l'élément html avec l'id "colors"
                     let productColors = document.getElementById("colors");
+                    //Variable pour créer un nouvel élément html <option>
                     let itemsColors = document.createElement("option");
+                    //Attribution d'une value et sa valeur à <option>
                     itemsColors.setAttribute("value", valueColors);
+                    //Attribution d'un contenu de type texte dans l'élément html
                     itemsColors.textContent = valueColors;
+                    //Filliation à productColors : itemsColors devient enfant de productColors
                     productColors.appendChild(itemsColors);
                 }
             }
@@ -53,8 +67,11 @@ function getId(url) {
 }
 getId(url)
 
+
 //variable de récupération de l'élément sur lequel on va appliquer une écoute d'évenement
-let ecouteBouton = document.getElementById("addToCart");
+var ecouteBouton = document.getElementById("addToCart");
+//Variable de récupération des données du localStorage
+var productInPanier = JSON.parse(localStorage.getItem("panier"));
 //création d'un modèle d'objet pour enregistrer toutes les valeurs dans un seul item
 class product {
     constructor(id, quantity, color) {
@@ -63,12 +80,11 @@ class product {
         this.color = color;
     }
 }
-//récupération des données du localStorage
-let productInPanier = JSON.parse(localStorage.getItem("panier"));
 //fonction pour ajouter un nouvel obj au localStorage sans multiplier les obj avec le même id et la même couleur
 function addPanier(){
-    //variables pour récuperer la couleur et la quantité choisie
+    //variables pour récuperer la couleur choisie
     let chooseColor = document.querySelector("#colors").value;
+    //variables pour récuperer la quantité choisie
     let chooseQuantity = document.querySelector("#quantity").value;
     //modifier les données string en number (parsInt(x,10), 10 pour une valeur en décimale)
     let quantityChooseQuantity = parseInt(chooseQuantity,10);
